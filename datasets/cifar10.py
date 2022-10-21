5ddd2df5f585f0d70b632c129ac9051f0b558846
PL97 <pengle@126.com>
2022-10-20 22:23:52 -0700

    update cifar10 & cifar100 dataloader

--- datasets/cifar10.py
+++ datasets/cifar10.py
@@ -1,7 +1,8 @@
-from torch.utils.data import sampler, DataLoader
+from torch.utils.data import sampler, DataLoader, Dataset, TensorDataset
 from torchvision import transforms
 from torchvision.datasets import CIFAR10, CIFAR100
 import numpy as np
+import torch
 
 import sys
 sys.path.append("../")
@@ -12,11 +13,21 @@ def get_cifar10_dataset():
             transforms.ToTensor(),
     ])
     
-    cifar10_dataset = DataLoader(CIFAR10(root="./data", \
-                                                train= True, \
-                                                transform=transform, download=True), \
-                                                batch_size=512, \
-                                                num_workers=8, \
-                                                persistent_workers=True, \
-                                                sampler=DatasetSampler(idx=list(range(50000)), frac=1))
-    return cifar10_dataset
+    cifar10_dataset = CIFAR10(root="./data", \
+                                train= True, \
+                                transform=transform, \
+                                download=True)
+    targets = cifar10_dataset.targets
+    targets = torch.IntTensor((np.asarray(targets)>=9).astype(int))
+    inputs = torch.tensor(cifar10_dataset.data.transpose(0, 3, 1, 2)).float()
+    
+    cifar10_dataset = TensorDataset(inputs, targets)
+                
+    cifar10_dataloader = DataLoader(cifar10_dataset,\
+                                        batch_size=512, \
+                                        num_workers=8, \
+                                        persistent_workers=True, \
+                                        sampler=DatasetSampler(idx=list(range(50000)), frac=1))
+    
+    
+    return cifar10_dataloader
--- datasets/cifar100.py
+++ datasets/cifar100.py
@@ -1,7 +1,8 @@
-from torch.utils.data import sampler, DataLoader
+from torch.utils.data import sampler, DataLoader, Dataset, TensorDataset
 from torchvision import transforms
 from torchvision.datasets import CIFAR10, CIFAR100
 import numpy as np
+import torch
 
 class DatasetSampler(sampler.Sampler):
     '''
@@ -21,30 +22,24 @@ class DatasetSampler(sampler.Sampler):
 
 
 
-def get_cifar10_dataset():
-    transform = transforms.Compose([
-            transforms.ToTensor(),
-    ])
-    
-    cifar10_dataset = DataLoader(CIFAR10(root="./data", \
-                                                train= True, \
-                                                transform=transform, download=True), \
-                                                batch_size=512, \
-                                                num_workers=8, \
-                                                persistent_workers=True, \
-                                                sampler=DatasetSampler(idx=list(range(50000)), frac=1))
-    return cifar10_dataset
 
 def get_cifar100_dataset():
     transform = transforms.Compose([
             transforms.ToTensor(),
     ])
     
-    cifar100_dataset = DataLoader(CIFAR100(root="./data", \
+    cifar100_dataset = CIFAR100(root="./data", \
                                                 train= True, \
-                                                transform=transform, download=True), \
-                                                batch_size=512, \
-                                                num_workers=8, \
-                                                persistent_workers=True, \
-                                                sampler=DatasetSampler(idx=list(range(50000)), frac=1))
-    return cifar100_dataset
+                                                transform=transform, download=True)
+    targets = cifar100_dataset.targets
+    targets = torch.IntTensor((np.asarray(targets)>=9).astype(int))
+    inputs = torch.tensor(cifar100_dataset.data.transpose(0, 3, 1, 2)).float()
+    
+    cifar100_dataset = TensorDataset(inputs, targets)
+                
+    cifar100_dataloader = DataLoader(cifar100_dataset,\
+                                        batch_size=512, \
+                                        num_workers=8, \
+                                        persistent_workers=True, \
+                                        sampler=DatasetSampler(idx=list(range(50000)), frac=1))
+    return cifar100_dataloader
